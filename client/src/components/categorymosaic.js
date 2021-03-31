6fd8b874893e6429f71b5d30fbf24665366c70f8
Michael <mgardner94@live.com>
2021-03-30 22:45:40 -0400

    foo

--- client/src/components/categorymosaic.js
+++ client/src/components/categorymosaic.js
@@ -21,16 +21,12 @@ export default class extends React.Component {
 
 
     render() {
-        let categories = ["Breakfast","Lunch","Dinner","Dessert","Snacks"]
-        let items = [...this.state.apiResponse];
-
-        // let categories = this.state.apiResponse;
         return (
             <div>
                 <div style={{ position: 'relative', textAlign: 'center', fontSize: "24px" }}>Categories</div>                  
 
                 <CardColumns style={{ position: 'relative', textAlign: 'center', fontSize: "24px" }}>
-                    {items.map(category => (
+                    {this.state.apiResponse.map(category => (
                         <Category category={category} />
                     ))}
                 </CardColumns>  
