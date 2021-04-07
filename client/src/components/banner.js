6748358f10c3a524769101f0dba1f4cac26d7a56
Dan <danchalifa@gmail.com>
2021-04-06 22:35:48 -0400

    Banner
    foo bar

--- client/src/components/banner.js
+++ client/src/components/banner.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import logo from '../images/tablecloth.jpeg'; // Tell webpack this JS file uses this image
-// import 'banner.css'
+import Banner from '../images/Banner_v1.jpg'; // Tell webpack this JS file uses this image
+import center from './banner.css'
 
 export default class extends React.Component {
 
     render() {
         return (
             
-            <div id={'bannerimage'}>
-                <div style={{ width: '100%', height: '50%', position: 'relative', textAlign: 'center', fontSize:"72px"}}>RECITAS de MAMA</div>   
-            </div>
+        <div>
+            <img src={Banner} alt="My_Logo" className="center"/>
+ 
+        </div>
+            
+
+
 
         )
     }
